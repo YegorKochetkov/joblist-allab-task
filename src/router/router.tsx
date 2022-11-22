ce85813ef12a4f726919dc23ca7749f228d29a17
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-11-23 01:38:14 +0200

    feat(router): add router

---
+++ src/router/router.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { createBrowserRouter, redirect } from 'react-router-dom';
+import App from '@/App';
+import JobDetails from '@/components/JobDetails';
+import ErrorPage from '@/components/ErrorPage';
+import PaginatedItems from '@/components/PaginatedItems';
+
+export const appRootPath = '/joblist-allab-task/';
+export const appJobsListPath = '/joblist-allab-task/jobsList/';
+export const appJobDetailsPath = '/joblist-allab-task/jobDetails/';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: appRootPath,
+		errorElement: <ErrorPage />,
+		loader: () => redirect(appJobsListPath + '1'),
+	},
+	{
+		path: appJobsListPath,
+		errorElement: <ErrorPage />,
+		loader: () => redirect(appJobsListPath + '1'),
+	},
+	{
+		path: appJobsListPath,
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: ':page',
+				element: <PaginatedItems />,
+				loader: ({ params }) => {
+					const parsedParams = Number(params.page);
+
+					if (!parsedParams) {
+						const error = {
+							status: 404,
+							statusText: 'Wrong address!',
+						};
+
+						throw error;
+					}
+				},
+				errorElement: <ErrorPage />,
+			},
+		],
+	},
+	{
+		path: appJobDetailsPath + ':jobId',
+		element: <JobDetails />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: appJobDetailsPath,
+		loader: ({ params }) => {
+			if (!params.jobId) {
+				const error = {
+					status: 404,
+					statusText: 'You should choose job!',
+				};
+
+				throw error;
+			}
+
+			return;
+		},
+		errorElement: <ErrorPage />,
+	},
+]);
+
+export default router;
