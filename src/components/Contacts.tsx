4bbf64a9a707a5c4e03385939fc8f49be0254863
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-11-26 11:44:17 +0200

    feat(map): add map to contacts

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,8 +1,12 @@
+import React, { useState } from 'react';
 import { JobType } from '@/types/job';
 import LocationIcon from '@/ui/LocationIcon';
 import { mapSearchLink } from '@/utils/urls';
-import React from 'react';
 import tw from 'twin.macro';
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+import ErrorMessage from './ErrorMessage';
+import Map, { Marker } from './GoogleMap';
+import Loader from './Loader';
 
 type ContactsAsideProps = {
 	job: JobType;
@@ -14,8 +18,7 @@ const ContactsContainer = tw.section`
 	py-8
 	text-appLightGrey
 	bg-appBackgroundContacts
-	rounded-lg
-	min-w-[25rem]
+	rounded-t-lg
 `;
 
 const Location = tw.a`
@@ -51,22 +54,69 @@ const Phone = tw.a`
 
 const Email = tw(Phone)``;
 
+const renderForMap = (status: Status) => {
+	if (Status.FAILURE) {
+		return <ErrorMessage error='Failure to load map' />;
+	}
+
+	if (Status.LOADING) {
+		return <Loader />;
+	}
+
+	return <h1>{status}</h1>;
+};
+
+const styleForMap = {
+	width: '100%',
+	height: '13rem',
+};
+
+const GoogleMap = tw.div`
+	rounded-b-lg
+	overflow-hidden
+	mb-6
+`;
+
+const ContactsWrapper = tw.div`
+	xl:mt-14
+	xl:max-w-[19rem]
+	2xl:max-w-[25rem]
+`;
+
 function Contacts({ job }: ContactsAsideProps) {
+	const markerPosition = { lat: job.location.lat, lng: job.location.long };
+	const [zoom] = React.useState(3);
+	const [center] = useState<google.maps.LatLngLiteral>(markerPosition);
+
 	return (
-		<ContactsContainer>
-			<JobName>
-				Department name.
+		<ContactsWrapper>
+			<ContactsContainer>
+				<JobName>
+					Department name.
+					<br />
+					{job.name + '.'}
+				</JobName>
+				<Address>
+					<LocationIcon />
+					<Location href={mapSearchLink + job.address.split(' ').join('+')}>
+						{job.address}
+					</Location>
+				</Address>
+				<Phone href={`tel:${job.phone}`}>{job.phone}</Phone>
 				<br />
-				{job.name + '.'}
-			</JobName>
-			<Address>
-				<LocationIcon />
-				<Location href={mapSearchLink + job.address}>{job.address}</Location>
-			</Address>
-			<Phone href={`tel:${job.phone}`}>{job.phone}</Phone>
-			<br />
-			<Email href={`mailto:${job.email}`}>{job.email}</Email>
-		</ContactsContainer>
+				<Email href={`mailto:${job.email}`}>{job.email}</Email>
+			</ContactsContainer>
+			<GoogleMap>
+				<Wrapper
+					apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}
+					render={renderForMap}
+				>
+					<Map center={center} zoom={zoom} style={styleForMap}>
+						<Marker position={markerPosition} job={job} />
+					</Map>
+				</Wrapper>
+			</GoogleMap>
+		</ContactsWrapper>
 	);
 }
 
